375bf6b5e5424411ce1c57de1bd955dd56f0e15c
SohelShahid117 <sohelshahid09@gmail.com>
2025-06-28 01:13:20 +0600

    194. Writing markup with JSX and Adding styles done

--- day_16_functional_component/src/App.jsx
+++ day_16_functional_component/src/App.jsx
@@ -22,9 +22,19 @@ export default App;
 import "./App.css";
 import Button from "./Button";
 const App = () => {
+  const name = "Shahid";
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>Hello World: {name}</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis, vero
+        <br />
+        deserunt error quod rem perspiciatis hic dolores recusandae molestiae
+        ipsam illo dolor laborum voluptatem fugit praesentium vitae iste? Nemo
+        impedit rem ducimus, architecto veritatis mollitia qui officia at odit,
+        iusto amet eveniet rerum ipsum repellendus repellat. Voluptatum harum
+        ratione autem.
+      </p>
       <Button />
     </>
   );
