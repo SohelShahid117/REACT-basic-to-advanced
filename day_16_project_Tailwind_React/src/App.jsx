5f98b5ab78ab9272c845cb1ebcb54177786fe720
SohelShahid117 <sohelshahid09@gmail.com>
2025-06-28 08:55:20 +0600

    195. How to Install Tailwind CSS on Your React Project done

---
+++ day_16_project_Tailwind_React/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex  justify-center">
+        <h1 className="text-3xl bg-orange-300 text-white p-5 text-center ">
+          Hello World
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
