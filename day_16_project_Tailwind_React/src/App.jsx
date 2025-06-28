52547fc32d12e8a686fe722ff34db9a94e0a126f
SohelShahid117 <sohelshahid09@gmail.com>
2025-06-28 09:26:33 +0600

    196. Displaying Data done

--- day_16_project_Tailwind_React/src/App.jsx
+++ day_16_project_Tailwind_React/src/App.jsx
@@ -1,12 +1,46 @@
 import "./App.css";
+import imgB from "./assets/b.jpg";
 
 function App() {
+  const name = "Sohel";
+  const user = {
+    name: "Hedy Lamarr",
+    imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+    imageSize: 90,
+  };
   return (
     <>
       <div className="flex  justify-center">
         <h1 className="text-3xl bg-orange-300 text-white p-5 text-center ">
-          Hello World
+          Hello {name}
         </h1>
+      </div>
+      {/* img from public folder */}
+      <img className="w-52 h-52" src="./a.jpg" alt="a" />
+
+      {/* img from src--asset folder  */}
+      <img className="w-60 h-60" src={imgB} alt="" />
+
+      <div>
+        <h2 className="mt-52 text-3xl font-bold">{user.name}</h2>
+
+        <img
+          className="w-52 h-52 mt-32 rounded-full ring-4  ring-red-500"
+          src={user.imageUrl}
+          alt=""
+        />
+
+        <img
+          style={{
+            width: user.imageSize + 50,
+            height: user.imageSize + 100,
+            // borderRadius: 10,
+            borderRadius: 50,
+            margin: 20,
+          }}
+          src={user.imageUrl}
+          alt=""
+        />
       </div>
     </>
   );
