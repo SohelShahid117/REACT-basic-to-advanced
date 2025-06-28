3b03afed5b112e7c9b2eb2b06a8608dd1e77c876
SohelShahid117 <sohelshahid09@gmail.com>
2025-06-28 16:38:13 +0600

    199. Responding to events done

--- day_17_rendering_lists/src/App.jsx
+++ day_17_rendering_lists/src/App.jsx
@@ -9,6 +9,10 @@ function App() {
 
   // const listItem = products.map((p) => <li key={p.id}>{p.title}</li>);
 
+  const myButton = () => {
+    alert("hello Sohel");
+  };
+
   return (
     <>
       <div className="text-4xl bg-orange-600 font-semibold p-5 text-white">
@@ -20,12 +24,21 @@ function App() {
           // <li key={p.id} style={{ color: p.isFruit ? "green" : "red" }}>
           <li
             key={p.id}
-            className={`${p.isFruit ? "text-green-500" : "text-blue-500"}`}
+            className={`${
+              p.isFruit ? "text-green-500" : "text-blue-500"
+            } text-3xl mb-4`}
           >
             {p.title}
           </li>
         ))}
       </ul>
+
+      <button
+        onClick={myButton}
+        className="bg-blue-400 px-4 py-2 rounded-2xl text-white text-2xl hover:scale-110"
+      >
+        Click Me
+      </button>
     </>
   );
 }
