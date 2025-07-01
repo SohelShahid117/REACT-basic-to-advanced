5d620b76b2ca0016352760af51e25e496bc184df
SohelShahid117 <sohelshahid09@gmail.com>
2025-07-01 10:35:45 +0600

    209. Handling Form Submissions and Validation part-01 done

--- day_18_19_handling form validation/src/components/Form.jsx
+++ day_18_19_handling form validation/src/components/Form.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log("form submit");
+    alert(`hi ${name}`);
+    console.log(name, email, message);
+    alert("thanks for submitting the form");
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
   return (
-    <form className="max-w-md bg-white text-black p-5 rounded space-y-5">
+    <form
+      onSubmit={handleFormSubmit}
+      className="max-w-md bg-white text-black p-5 rounded space-y-5"
+    >
       <div>
         <label className="block mb-1 text-xl" htmlFor="name">
           Name :
         </label>
         <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
           type="text"
           name="name"
           id="name"
@@ -20,6 +39,8 @@ const Form = () => {
           Email :
         </label>
         <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           type="text"
           name="email"
           id="email"
@@ -32,6 +53,10 @@ const Form = () => {
           Message :
         </label>
         <textarea
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          // value={message}
+          // onChange={(e) => setMessage(e.target.value)}
           className="border"
           name="message"
           id="message"
